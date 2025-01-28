9497366a95d0cac54e00ce273f492accf482869b
Sreedev <145640663+sreedev11@users.noreply.github.com>
2025-01-28 18:48:05 +0530

    Add files via upload

---
+++ Models/CancelledTable.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantManage.Models
+{
+    public class CancelledTable
+    {
+        public int TableId { get; set; }
+        public int ID { get; set; }     
+        public int Amount { get; set; } 
+        public DateTime CreatedDate { get; set; } 
+    }
+}
